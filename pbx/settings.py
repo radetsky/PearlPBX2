05bcac46f9aed655fa252cd7166a634a4d6a1b76
Alex Radetsky <alexey.radetsky@gmail.com>
2026-03-01 11:21:56 +0100

    Fixing the timezone Kyiv

--- pbx/settings.py
+++ pbx/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "Europe/Kiev"
+TIME_ZONE = "Europe/Kyiv"
 USE_I18N = True
 USE_TZ = True
 
